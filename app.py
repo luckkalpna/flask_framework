868e5a8a3d77b95960af761bb4ed55090d903bb3
luckkalpna <luckkalpna@gmail.com>
2025-04-12 18:40:05 +0530

    Add Delete and Update operation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,11 +15,14 @@ class Todo(db.Model):
     def __repr__(self):
         return f"{self.sno} - {self.title}"
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    todo = Todo(title="first title", desc="start investing in Stock Market")
-    db.session.add(todo)
-    db.session.commit()
+    if request.method=='POST':
+      title = request.form["title"]
+      desc = request.form["desc"]
+      todo = Todo(title=title, desc=desc)
+      db.session.add(todo)
+      db.session.commit()
     allTodo = Todo.query.all()
     return render_template("index.html", allTodo=allTodo)
     # return "<p>Hello, World!</p>"
@@ -29,6 +32,30 @@ def products():
     allTodo = Todo.query.all()  # for print all tables in databse
     print(allTodo)
     return "<p>this is an products page</p>"
+
+
+@app.route("/update/<int:sno>", methods=["GET", "POST"])
+def update(sno):
+    if request.method=='POST':
+      title = request.form["title"]
+      desc = request.form["desc"]
+      todo = Todo.query.filter_by(sno=sno).first()
+      todo.title = title
+      todo.desc = desc
+      db.session.add(todo)
+      db.session.commit()
+      return redirect("/")
+    
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", todo=todo)
+
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
