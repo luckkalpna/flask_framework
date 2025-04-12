c83ecfe863a568c3c87e46cda96433cbc7b31760
luckkalpna <luckkalpna@gmail.com>
2025-04-12 13:37:49 +0530

    Create data base using sqlachemny

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
 
 @app.route("/")
 def hello_world():
+    todo = Todo(title="first title", desc="start investing in Stock Market")
+    db.session.add(todo)
+    db.session.commit()
     return render_template("index.html")
     # return "<p>Hello, World!</p>"
 
